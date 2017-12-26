f7271422c93055982b61abfff72c46ed87da822d
Ameny <bouzidameny@outlook.com>
2017-12-26 23:50:43 +0100

    server-setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose= require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+mongoose.Promise=global.Promise ;
+mongoose.connect(config.uri,(err)=> {
+    if(err)
+    {
+        console.log('could not connect to datebase:',err) ;
+    }else {
+               console.log('connected '+config.db) ;
+    }
+})   ;
+app.use(express.static(__dirname + '/client/dist/'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'client/dist/index.html'));
+  });
+
+  app.listen(8080,() => {
+      console.log('Listening on port 8080')
+  }) ;
